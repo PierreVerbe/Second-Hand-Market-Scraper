f0a6081832b8667151f444414ca87c5a379618d8
Pierre Verbe <pierre.verbe@free.fr>
2021-01-31 23:28:34 +0100

    first scrape

--- SecondHandMarketScraper/SecondHandMarketScraper/spiders/leparking-moto/triumph_daytona_spider.py
+++ SecondHandMarketScraper/SecondHandMarketScraper/spiders/leparking-moto/triumph_daytona_spider.py
@@ -7,18 +7,24 @@ from scrapy.utils.log import configure_logging
 
 class TriumphDaytonaSpider(scrapy.Spider):
     name = 'LPM_Triumph_Daytona_Spider'
-
     custom_settings = {
-        'DOWNLOAD_DELAY': 2,
+        'DOWNLOAD_DELAY': 3,
+        'COOKIES_ENABLED': False,
     }
 
     lua_script="""
-
+        function main(splash, args)
+            assert(splash:go(args.url))
+            assert(splash:wait(5))
+            return {
+                html = splash:html(),
+            }
+        end
     """
 
     def start_requests(self):
         urls = [
-            'https://www.leparking-moto.fr/moto-occasion/triumph-daytona-675-france.html#!/moto-occasion/triumph-daytona-675.html%3Ftri%3Ddate',
+            'https://www.leparking-moto.fr/moto-occasion/triumph-daytona-675.html#!/moto-occasion/triumph-daytona-675.html%3Fslider_millesime%3D2013%7C2021%26tri%3Ddate',
         ]
         for url in urls:
             yield SplashRequest(url=url, callback=self.parse, args={
@@ -26,10 +32,9 @@ class TriumphDaytonaSpider(scrapy.Spider):
                 'wait': 3,
                 'lua_source': self.lua_script
                 })
-    
 
     def parse(self, response):
-        data = self.parse_item
+        items = self.parse_item
 
         # Works !
         self.logger.debug("hello")
@@ -46,7 +51,7 @@ class TriumphDaytonaSpider(scrapy.Spider):
         # Write into a file
         filename = f'lpm.json'
         with open(filename, 'wb') as f:
-            #f.write(data)
+            #f.write(items)
             f.write(response.body)
         self.log(f'Saved file {filename}')
 
@@ -62,16 +67,12 @@ class TriumphDaytonaSpider(scrapy.Spider):
                 'price': element.xpath('div[@class="info-comp clearfix"]/div[@class="price-block "]/p/text()').get(),
             }
      
-"""
-response.xpath('//ul[@id="resultats"]/li/section[@class="clearfix complete-holder"]/div[@class="padd-bloc clearfix"]/di 
-   ...: v[@class="info-comp clearfix"]/div[@class="price-block "]/p/text()').getall()
-   """
+
+# response.xpath('//ul[@id="resultats"]/li/section[@class="clearfix complete-holder"]/div[@class="padd-bloc clearfix"]/div[@class="info-comp clearfix"]/div[@class="price-block "]/p/text()').getall()
+
 
 class test(scrapy.Spider):
     name = 'test'
-
-
-
 
 # Run spiders sequentially
 configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
@@ -85,3 +86,33 @@ def crawl():
 
 crawl()
 reactor.run() # the script will block here until the last crawl call is finished
+
+
+"""
+Avoiding getting banned
+
+Some websites implement certain measures to prevent bots from crawling them, with varying degrees of sophistication. Getting around those measures can be difficult and tricky, and may sometimes require special infrastructure. Please consider contacting commercial support if in doubt.
+
+Here are some tips to keep in mind when dealing with these kinds of sites:
+
+    rotate your user agent from a pool of well-known ones from browsers (google around to get a list of them)
+
+    disable cookies (see COOKIES_ENABLED) as some sites may use cookies to spot bot behaviour
+
+    use download delays (2 or higher). See DOWNLOAD_DELAY setting.
+
+    if possible, use Google cache to fetch pages, instead of hitting the sites directly
+
+    use a pool of rotating IPs. For example, the free Tor project or paid services like ProxyMesh. An open source alternative is scrapoxy, a super proxy that you can attach your own proxies to.
+
+    use a highly distributed downloader that circumvents bans internally, so you can just focus on parsing clean pages. One example of such downloaders is Crawlera
+
+"""
+
+# scrapy shell 'http://localhost:8050/render.html?url=https://www.leparking-moto.fr/moto-occasion/triumph-daytona-675-france.html#!/moto-occasion/triumph-daytona-675.html%3Ftri%3Ddate&timeout=10&wait=0.5'
+
+"""
+idees
+selenium
+proxuy agent
+"""
